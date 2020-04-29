d2ec8c02db187924830935a54f5d609ff39f153e
Jason Hitching <jasonhitching1994@gmail.com>
2020-04-29 09:04:36 +0100

    Changed animation and css shape properties to be a square

--- Animations/animation.js
+++ Animations/animation.js
@@ -1,21 +1,22 @@
-const { styler, spring, listen, pointer, value } = window.popmotion;
+const { easing, keyframes, styler } = window.popmotion;
 
-const ball = document.querySelector(".box");
-const divStyler = styler(ball);
-const ballXY = value({ x: 0, y: 0 }, divStyler.set);
+const divStyler = styler(document.querySelector(".box"));
 
-listen(ball, "mousedown touchstart").start((e) => {
-  e.preventDefault();
-  pointer(ballXY.get()).start(ballXY);
-});
-
-listen(document, "mouseup touchend").start(() => {
-  spring({
-    from: ballXY.get(),
-    velocity: ballXY.getVelocity(),
-    to: { x: 0, y: 0 },
-    stiffness: 200,
-    // mass: 1,
-    // damping: 10
-  }).start(ballXY);
-});
+keyframes({
+  values: [
+    { x: 0, y: 0, rotateY: 0, background: "#9B65DE" },
+    { x: 300, y: 0, rotateY: 180, rotateX: 0, background: "#14D790" },
+    { x: 300, y: 200, rotateY: 180, rotateX: 180, background: "#FF1C68" },
+    { x: 0, y: 200, rotateY: 0, rotateX: 180, background: "#198FE3" },
+    { x: 0, y: 0, rotateY: 0, rotateX: 0, background: "#9B65DE" },
+  ],
+  duration: 3000,
+  easings: [
+    easing.easeInOut,
+    easing.easeInOut,
+    easing.easeInOut,
+    easing.easeInOut,
+  ],
+  loop: Infinity,
+  //times: [0, 0.2, 0.5, 0.6, 1]
+}).start(divStyler.set);
